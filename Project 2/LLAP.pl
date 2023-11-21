7f15a3aa210a68fce8ff06ae61f8df8c963ea7dd
Omar_Mamdouh <omar.mamdouh@techsa.io>
2023-11-21 15:43:30 +0200

    Detects true and false but infinite loops when generating path.

--- Project 2/LLAP.pl
+++ Project 2/LLAP.pl
@@ -1,199 +1,7 @@
 :-include('KB.pl').
 
-getCurrentFood(Situation,OutputFood):-
-    food(InitialFood),
-    getCurrentFoodHelper(Situation,InitialFood,OutputFood).
-
-getCurrentFoodHelper(s0, CurrentFood, CurrentFood).
-
-getCurrentFoodHelper(result(Action,Situation),CurrentFood,OutputFood):-
-    Action == reqf,
-    NewFood is CurrentFood + 1,
-    getCurrentFoodHelper(Situation,NewFood,OutputFood).
-
-getCurrentFoodHelper(result(Action,Situation),CurrentFood,OutputFood):-
-    Action == b1,
-    build1(RequiredFood,_,_),
-    NewFood is CurrentFood - RequiredFood,
-    getCurrentFoodHelper(Situation,NewFood,OutputFood).
-
-getCurrentFoodHelper(result(Action,Situation),CurrentFood,OutputFood):-
-    Action == b2,
-    build2(RequiredFood,_,_),
-    NewFood is CurrentFood - RequiredFood,
-    getCurrentFoodHelper(Situation,NewFood,OutputFood).
-
-getCurrentFoodHelper(result(Action,Situation),CurrentFood,OutputFood):-
-    Action \= reqf,
-    Action \= b1,
-    Action \= b2,
-    getCurrentFoodHelper(Situation,CurrentFood,OutputFood).
-
-
-getCurrentEnergy(Situation,OutputEnergy):-
-    energy(InitialEnergy),
-    getCurrentEnergyHelper(Situation,InitialEnergy,OutputEnergy).
-
-getCurrentEnergyHelper(s0, CurrentEnergy, CurrentEnergy).
-
-getCurrentEnergyHelper(result(Action,Situation),CurrentEnergy,OutputEnergy):-
-    Action == reqe,
-    NewEnergy is CurrentEnergy + 1,
-    getCurrentEnergyHelper(Situation,NewEnergy,OutputEnergy).
-
-getCurrentEnergyHelper(result(Action,Situation),CurrentEnergy,OutputEnergy):-
-    Action == b1,
-    build1(_,_,RequiredEnergy),
-    NewEnergy is CurrentEnergy - RequiredEnergy,
-    getCurrentEnergyHelper(Situation,NewEnergy,OutputEnergy).
-
-getCurrentEnergyHelper(result(Action,Situation),CurrentEnergy,OutputEnergy):-
-    Action == b2,
-    build2(_,_,RequiredEnergy),
-    NewEnergy is CurrentEnergy - RequiredEnergy,
-    getCurrentEnergyHelper(Situation,NewEnergy,OutputEnergy).
-
-getCurrentEnergyHelper(result(Action,Situation),CurrentEnergy,OutputEnergy):-
-    Action \= reqe,
-    Action \= b1,
-    Action \= b2,
-    getCurrentEnergyHelper(Situation,CurrentEnergy,OutputEnergy).
-
-
-
-getCurrentMaterials(Situation,OutputMaterials):-
-    materials(InitialMaterials),
-    getCurrentMaterialsHelper(Situation,InitialMaterials,OutputMaterials).
-
-getCurrentMaterialsHelper(s0, CurrentMaterials, CurrentMaterials).
-
-getCurrentMaterialsHelper(result(Action,Situation),CurrentMaterials,OutputMaterials):-
-    Action == reqm,
-    NewMaterials is CurrentMaterials + 1,
-    getCurrentMaterialsHelper(Situation,NewMaterials,OutputMaterials).
-
-getCurrentMaterialsHelper(result(Action,Situation),CurrentMaterials,OutputMaterials):-
-    Action == b1,
-    build1(_,RequiredMaterials,_),
-    NewMaterials is CurrentMaterials - RequiredMaterials,
-    getCurrentMaterialsHelper(Situation,NewMaterials,OutputMaterials).
-
-getCurrentMaterialsHelper(result(Action,Situation),CurrentMaterials,OutputMaterials):-
-    Action == b2,
-    build2(_,RequiredMaterials,_),
-    NewMaterials is CurrentMaterials - RequiredMaterials,
-    getCurrentMaterialsHelper(Situation,NewMaterials,OutputMaterials).
-
-getCurrentMaterialsHelper(result(Action,Situation),CurrentMaterials,OutputMaterials):-
-    Action \= reqm,
-    Action \= b1,
-    Action \= b2,
-    getCurrentMaterialsHelper(Situation,CurrentMaterials,OutputMaterials).
-
-
-max(X, Y, X) :- X >= Y, !.
-max(X, Y, Y) :- X < Y.
-
-getCurrentBuild1Count(Situation,OutputBuild1Count):-
-    getCurrentBuild1CountHelper(Situation,0,OutputBuild1Count).
-
-getCurrentBuild1CountHelper(s0, CurrentBuild1Count, CurrentBuild1Count).
-
-getCurrentBuild1CountHelper(result(Action,Situation),CurrentBuild1Count,OutputBuild1Count):-
-    Action == b1,
-    NewBuild1Count is CurrentBuild1Count + 1,
-    getCurrentBuild1CountHelper(Situation,NewBuild1Count,OutputBuild1Count).
-
-getCurrentBuild1CountHelper(result(Action,Situation),CurrentBuild1Count,OutputBuild1Count):-
-    Action \= b1,
-    getCurrentBuild1CountHelper(Situation,CurrentBuild1Count,OutputBuild1Count).
-
-getCurrentBuild2Count(Situation,OutputBuild2Count):-
-    getCurrentBuild2CountHelper(Situation,0,OutputBuild2Count).
-
-getCurrentBuild2CountHelper(s0, CurrentBuild2Count, CurrentBuild2Count).
-
-getCurrentBuild2CountHelper(result(Action,Situation),CurrentBuild2Count,OutputBuild2Count):-
-    Action == b2,
-    NewBuild2Count is CurrentBuild2Count + 1,
-    getCurrentBuild2CountHelper(Situation,NewBuild2Count,OutputBuild2Count).
-
-getCurrentBuild2CountHelper(result(Action,Situation),CurrentBuild2Count,OutputBuild2Count):-
-    Action \= b2,
-    getCurrentBuild2CountHelper(Situation,CurrentBuild2Count,OutputBuild2Count).
-
-
-applyAction(Action, Situation, result(Action,Situation)):-
-    getCurrentFood(Situation,CurrentFood),
-    getCurrentMaterials(Situation,CurrentMaterials),
-    getCurrentEnergy(Situation,CurrentEnergy),
-    getCurrentBuild1Count(Situation,CurrentBuild1Count),
-    CurrentBuild1Count == 0,
-    build1(RequiredFood,RequiredMaterials,RequiredEnergy),
-    CurrentFood >= RequiredFood,
-    CurrentMaterials >= RequiredMaterials,
-    CurrentEnergy >= RequiredEnergy,
-    Action = b1.
-
-applyAction(Action, Situation, result(Action,Situation)):-
-    getCurrentFood(Situation,CurrentFood),
-    getCurrentMaterials(Situation,CurrentMaterials),
-    getCurrentEnergy(Situation,CurrentEnergy),
-    getCurrentBuild2Count(Situation,CurrentBuild2Count),
-    CurrentBuild2Count == 0,
-    build2(RequiredFood,RequiredMaterials,RequiredEnergy),
-    CurrentFood >= RequiredFood,
-    CurrentMaterials >= RequiredMaterials,
-    CurrentEnergy >= RequiredEnergy,
-    Action = b2.
-
-applyAction(Action, Situation, result(Action,Situation)):-
-    Action = reqf,
-    getCurrentFood(Situation,CurrentFood),
-    build1(RequiredFood1,_,_),
-    build2(RequiredFood2,_,_),
-    max(RequiredFood1,RequiredFood2,RequiredFood),
-    CurrentFood < RequiredFood.
-    
-
-applyAction(Action, Situation, result(Action,Situation)):-
-    Action = reqm,
-    getCurrentMaterials(Situation,CurrentMaterials),
-    build1(_,RequiredMaterials1,_),
-    build2(_,RequiredMaterials2,_),
-    max(RequiredMaterials1,RequiredMaterials2,RequiredMaterials),
-    CurrentMaterials < RequiredMaterials.
-
-applyAction(Action, Situation, result(Action,Situation)):-
-    Action = reqe,
-    getCurrentEnergy(Situation,CurrentEnergy),
-    build1(_,_,RequiredEnergy1),
-    build2(_,_,RequiredEnergy2),
-    max(RequiredEnergy1,RequiredEnergy2,RequiredEnergy),
-    CurrentEnergy < RequiredEnergy.
-
-goalState(Situation):-
-    goalStateHelper(Situation, 0,0).
-
-goalStateHelper(s0, Build1Counter, Build2Counter):- 
-    Build1Counter>0, 
-    Build2Counter>0.
-goalStateHelper(Situation, Build1Counter, Build2Counter):-
-    Situation = result(b1, newSituation),
-    NewBuild1Counter is Build1Counter + 1,
-    goalStateHelper(newSituation, NewBuild1Counter, Build2Counter).
-goalStateHelper(Situation, Build1Counter, Build2Counter):-
-    Situation = result(b2, newSituation),
-    NewBuild2Counter is Build2Counter + 1,
-    goalStateHelper(newSituation, Build1Counter, NewBuild2Counter).
-goalStateHelper(Situation, Build1Counter, Build2Counter):-
-    Situation = result(Action, newSituation),
-    Action \= b1,
-    Action \= b2,
-    goalStateHelper(newSituation, Build1Counter, Build2Counter).
-
 goal(Situation):-
-    ids(Situation,1).
+    ids(Situation,20).
 
 ids(Situation,Length):-
     (call_with_depth_limit(goalHelper(Situation),Length,R), number(R));
@@ -201,8 +9,61 @@ ids(Situation,Length):-
     Length1 is Length+1, ids(Situation,Length1)).
 
 goalHelper(Situation):-
-    goalState(Situation).
+    goalLogic(_,BuildStatus,Situation),
+    BuildStatus = build(Build1Counter,Build2Counter),
+    Build1Counter > 0,
+    Build2Counter > 0.
 
-goalHelper(Situation):-
-    applyAction(Action, Situation, ResultingSituation),
-    goalHelper(ResultingSituation).
+goalLogic(InitialResources,InitialBuildStatus,s0):-
+    food(InitialFood),
+    materials(InitialMaterials),
+    energy(InitialEnergy),
+    InitialResources = resource(InitialFood,InitialMaterials,InitialEnergy),
+    InitialBuildStatus = build(0,0).
+
+goalLogic(Resources,build(Build1Counter,Build2Counter),result(b1,Situation)):-
+    goalLogic(PreviousResources,BuildStatus,Situation),
+    build(PreviousBuild1Counter,Build2Counter) = BuildStatus,
+    resource(PreviousFood,PreviousMaterials,PreviousEnergy) = PreviousResources,
+    build1(RequiredBuild1Food,RequiredBuild1Materials,RequiredBuild1Energy),
+    RequiredBuild1Food =< PreviousFood,
+    RequiredBuild1Materials =< PreviousMaterials,
+    RequiredBuild1Energy =< PreviousEnergy,
+    Build1Counter is PreviousBuild1Counter + 1,
+    ResultingFood is PreviousFood - RequiredBuild1Food,
+    ResultingMaterials is PreviousMaterials - RequiredBuild1Materials,
+    ResultingEnergy is PreviousEnergy - RequiredBuild1Energy,
+    Resources = resource(ResultingFood, ResultingMaterials,ResultingEnergy).
+
+goalLogic(Resources,build(Build1Counter,Build2Counter),result(b2,Situation)):-
+    goalLogic(PreviousResources,BuildStatus,Situation),
+    build(Build1Counter,PreviousBuild2Counter) = BuildStatus,
+    resource(PreviousFood,PreviousMaterials,PreviousEnergy) = PreviousResources,
+    build2(RequiredBuild2Food,RequiredBuild2Materials,RequiredBuild2Energy),
+    RequiredBuild2Food =< PreviousFood,
+    RequiredBuild2Materials =< PreviousMaterials,
+    RequiredBuild2Energy =< PreviousEnergy,
+    Build2Counter is PreviousBuild2Counter + 1,
+    ResultingFood is PreviousFood - RequiredBuild2Food,
+    ResultingMaterials is PreviousMaterials - RequiredBuild2Materials,
+    ResultingEnergy is PreviousEnergy - RequiredBuild2Energy,
+    Resources = resource(ResultingFood, ResultingMaterials,ResultingEnergy).
+
+goalLogic(Resources,BuildStatus,result(reqf,Situation)):-
+    goalLogic(PreviousResources,BuildStatus,Situation),
+    resource(PreviousFood,PreviousMaterials,PreviousEnergy) = PreviousResources,
+    ResultingFood is PreviousFood + 1,
+    Resources = resource(ResultingFood, PreviousMaterials,PreviousEnergy).
+
+goalLogic(Resources,BuildStatus,result(reqm,Situation)):-
+    goalLogic(PreviousResources,BuildStatus,Situation),
+    resource(PreviousFood,PreviousMaterials,PreviousEnergy) = PreviousResources,
+    ResultingMaterials is PreviousMaterials + 1,
+    Resources = resource(PreviousFood, ResultingMaterials ,PreviousEnergy).
+
+goalLogic(Resources,BuildStatus,result(reqe,Situation)):-
+    goalLogic(PreviousResources,BuildStatus,Situation),
+    resource(PreviousFood,PreviousMaterials,PreviousEnergy) = PreviousResources,
+    ResultingEnergy is PreviousEnergy + 1,
+    Resources = resource(PreviousFood, PreviousMaterials,ResultingEnergy).
+
